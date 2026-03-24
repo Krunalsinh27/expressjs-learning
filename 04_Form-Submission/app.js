c724b8c7c9f58f50a3e997d670de7b5033a8c3ae
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-03-24 11:27:38 +0530

    Initialize Express app for form submission

---
+++ 04_Form-Submission/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+const staticPath = path.join(import.meta.dirname, 'public');
+app.use(express.static(staticPath));
+
+app.use(express.urlencoded({extended: true}));
+
+app.post("/contact", (req, res) => {
+    console.log(req.body);
+    res.redirect("/");    
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
